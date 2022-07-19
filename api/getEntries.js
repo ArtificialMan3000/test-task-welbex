384439466cc612f08a235bba33ffc2beb34b9bd3
Alexander Bosikov <legionerab3000@yandex.ru>
2022-07-19 17:13:20 +0300

    Add order clause to query string

--- api/getEntries.js
+++ api/getEntries.js
@@ -1,7 +1,5 @@
-const getEntries = async (dbClient, { filter, offset, limit } = {}) => {
-  filter = filter || [];
-
-  const whereClause = filter.length
+const getEntries = async (dbClient, { filter, offset, limit, order } = {}) => {
+  const whereClause = filter?.length
     ? filter.reduce((where, filterItem, index) => {
         const and = index < filter.length - 1 ? 'AND' : '';
         return `${where} ${filterItem.name} ${filterItem.sign} ${filterItem.value} ${and}`;
@@ -9,12 +7,14 @@ const getEntries = async (dbClient, { filter, offset, limit } = {}) => {
     : '';
   const offsetClause = offset ? `OFFSET ${offset}` : '';
   const limitClause = limit ? `LIMIT ${limit}` : '';
+  const orderClause = order ? `ORDER BY ${order.name} ${order.direction}` : '';
 
-  const res = await dbClient.query(
-    `SELECT * FROM "task_table" ${whereClause} ${limitClause} ${offsetClause}`
-  );
-  console.log(res);
+  const queryString = `SELECT * FROM "task_table" ${whereClause} ${limitClause} ${offsetClause} ${orderClause}`;
+  console.log(queryString);
+
+  const res = await dbClient.query(queryString);
+  // console.log(res);
   return res;
-};
+};;
 
 module.exports = { getEntries };
